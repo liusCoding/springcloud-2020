d050050eda25f69a64faceda9327f896c73e489d
liusCoding <14786469221@163.com>
2020-06-03 23:23:26 +0800

    2.支付模块构建

---
+++ cloud-provider-payment-8001/src/main/java/com/liuscoding/springcloud/PaymentApplication.java
@@ -0,0 +1,17 @@
+package com.liuscoding.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @className: PayMentApplication
+ * @description:
+ * @author: liusCoding
+ * @create: 2020-06-03 23:06
+ */
+@SpringBootApplication
+public class PaymentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication.class,args);
+    }
+}

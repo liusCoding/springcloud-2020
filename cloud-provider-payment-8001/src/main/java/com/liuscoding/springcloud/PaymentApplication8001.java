0304d3ffa1889a4ee4178d88e2798691272f326d
liusCoding <14786469221@163.com>
2020-06-04 15:44:30 +0800

    7.支付微服务8001入驻进EurekaServer

--- cloud-provider-payment-8001/src/main/java/com/liuscoding/springcloud/PaymentApplication8001.java
+++ cloud-provider-payment-8001/src/main/java/com/liuscoding/springcloud/PaymentApplication8001.java
@@ -2,6 +2,7 @@ package com.liuscoding.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @className: PayMentApplication
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create: 2020-06-03 23:06
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentApplication8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentApplication8001.class,args);

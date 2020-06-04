cc34e3aa665a4b47d474aea0e385eb16a8288eea
liusCoding <14786469221@163.com>
2020-06-04 15:53:55 +0800

    8.订单微服务80入驻进EurekaServer

--- cloud-consumer-order-80/src/main/java/com/liuscoding/springcloud/OrderApplication80.java
+++ cloud-consumer-order-80/src/main/java/com/liuscoding/springcloud/OrderApplication80.java
@@ -2,6 +2,7 @@ package com.liuscoding.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @className: OrderApp80
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create: 2020-06-04 11:48
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderApplication80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication80.class,args);

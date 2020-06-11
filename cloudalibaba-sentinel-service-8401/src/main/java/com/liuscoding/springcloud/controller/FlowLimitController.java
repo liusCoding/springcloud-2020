cafa45af75bfbf53101221857a93d6f728b34035
liusCoding <14786469221@163.com>
2020-06-11 13:51:01 +0800

    50.sentinel 限流配置

--- cloudalibaba-sentinel-service-8401/src/main/java/com/liuscoding/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service-8401/src/main/java/com/liuscoding/springcloud/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.liuscoding.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -25,6 +28,40 @@ public class FlowLimitController {
     {
         log.info(Thread.currentThread().getName()+"\t"+"...testB");
         return "------testB";
+    }
+
+
+    @GetMapping("/testD")
+    public String testD()
+    {
+//        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+//        log.info("testD 测试RT");
+
+        log.info("testD 异常比例");
+        int age = 10/0;
+        return "------testD";
+    }
+
+
+    @GetMapping("/testE")
+    public String testE()
+    {
+        log.info("testE 测试异常数");
+        int age = 10/0;
+        return "------testE 测试异常数";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p2",required = false) String p2)
+    {
+        //int age = 10/0;
+        return "------testHotKey";
+    }
+    public String deal_testHotKey (String p1, String p2, BlockException exception)
+    {
+        return "------deal_testHotKey,o(╥﹏╥)o";  //sentinel系统默认的提示：Blocked by Sentinel (flow limiting)
     }
 
 
